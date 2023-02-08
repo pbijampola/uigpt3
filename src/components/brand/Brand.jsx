5019f3335362b9ad69832613ce5ce00466970698
PATRICK BIJAMPOLA <patrickbijampola@gmail.com>
2023-02-08 07:27:33 +0300

    brand section done

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './brand.css';
+import{ google,slack,dropbox,shopify,atlassian} from './imports'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="Google" srcset="" />
+      </div>
+      <div>
+        <img src={slack} alt="Slack" srcset="" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Dropbox" srcset="" />
+      </div>
+      <div>
+        <img src={shopify} alt="Shopify" srcset="" />
+      </div>
+      <div>
+        <img src={atlassian} alt="Atlassian" srcset="" />
+      </div>
+    </div>
   )
 }
 
